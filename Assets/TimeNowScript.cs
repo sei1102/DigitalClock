498503ca7b1469c59cb25be3529c9bf1762c41a4
Satoshi Nakanishi <aq.b.b.7.6@gmail.com>
2020-03-01 15:33:48 +0900

    First Commit

---
+++ Assets/TimeNowScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+
+public class TimeNowScript : MonoBehaviour
+{
+    private TextMeshPro ClockText;
+
+    // Use this for initialization
+    void Start()
+    {
+        ClockText = this.GetComponent<TextMeshPro>();
+        ClockText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClockText.text = DateTime.Now.ToLongTimeString();
+    }
+}
